47da4faa128c8fd5f9c82d118fb7bed4601667e1
g4reen <ba.sepulvedat@duocuc.cl>
2025-05-29 12:42:50 -0400

    cambio en requestmapping

--- src/main/java/com/example/perfulandiaspa/controller/PagoController.java
+++ src/main/java/com/example/perfulandiaspa/controller/PagoController.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/pagos")
+@RequestMapping("/api/v1/pagos")
 public class PagoController {
     private final PagoService pagoService;
 
